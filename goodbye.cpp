54defae1b9e241cb4e0767db930e43650cdb55fa
Khalid_hossain <echo.khalidhossain@gmail.com>
2026-04-13 19:43:17 +0600

    messed2

--- goodbye.cpp
+++ goodbye.cpp
@@ -9,11 +9,9 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-<<<<<<< HEAD:goodbye.cpp
+
     cout << "hello world";
-=======
-    cout << "hello world!";
->>>>>>> 3709dd4f6435e9d92a45178f864b567999658573:hello.cpp
+
     
     return 0;
 }
