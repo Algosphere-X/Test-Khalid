72da73059278411fa85580e5d4e4f6081dfaa2ce
Khalid_hossain <echo.khalidhossain@gmail.com>
2026-04-13 20:14:48 +0600

    making some changes because it was causing trouble to fetch files from main

--- goodbye.cpp
+++ goodbye.cpp
@@ -1,17 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-    /* Write the Problem Description 
-    Link Address: 
-    */
-
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
     cout << "hello world";
-
+    int x=100;
+    cout << "\n" << x ;
     
     return 0;
 }
